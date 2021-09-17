e0fb147032138f8884b26e7042aee4bb06d5835a
Yamini Patidar <yaminipatidar20@gmail.com>
2021-09-17 12:08:17 +0530

    Add files via upload

---
+++ my_site/blogs/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status="published")
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
+        null=True,
+    )
+    content = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    objects = models.Manager()  # Default Manager
+    published = PublishedManager()  # Custom Manager
+
+    class Meta:
+        ordering = ("-published_at",)
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
