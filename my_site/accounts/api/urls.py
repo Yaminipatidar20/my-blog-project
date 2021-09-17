23982eb11d3d9ef64aeacaa255a324708ce93f2e
Yamini Patidar <yaminipatidar20@gmail.com>
2021-09-17 11:45:27 +0530

    Delete my_site/accounts directory

--- my_site/accounts/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("create/", views.user_create_api_view, name="user-create-api"),
-    
-]
--- my_site/accounts/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import CreateAPIView
-from .serializers import UserCreateSerializer
-
-class UserCreateAPIView(CreateAPIView):
-    serializer_class = UserCreateSerializer
-
-user_create_api_view = UserCreateAPIView().as_view()
-
